636219bba080b59143706b7e8d64609796451784
deenbrembo <hafizudinmuhammad83@gmail.com>
2024-01-06 05:55:54 +0800

    completed

--- index.js
+++ index.js
@@ -603,7 +603,7 @@ async function login(client, data) {
         case "Admin":
           return "You are logged in as Admin\n1) Register Security\n2) Dump or Read All Hosts Data\n\nToken for " + match.name + ": " + token + "\n";
         case "Security":
-          return "You are logged in as Security\n1) register Host\n2) Retrieve Host's PhoneNumber from Visitor Pass\n\nToken for " + match.name + ": " + token + "\n";
+          return "You are logged in as Security\n1) register Host\n2) Retrieve Hosts PhoneNumber from Visitor Pass\n\nToken for " + match.name + ": " + token + "\n";
         case "Host":
           return "You are logged in as a Host User\n1) Read All Visitor\n2) Issue the Pass for Visitor\n\nToken for " + match.name + ": " + token + "\n";
         default:
